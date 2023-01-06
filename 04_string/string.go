d17d11650c6d2832b7b67baa26e78bd0bbd5cdf4
Krossmanzs <radi.adi78@gmail.com>
2023-01-06 17:03:29 +0700

    string

---
+++ 04_string/string.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+/*
+	Menghitung jumlah karakter di String:
+		len("string")
+	Mengambil karakter pada posisi yang ditentukan:
+		"string"[number]
+*/
+
+func main() {
+	fmt.Println("linux"[3])
+	fmt.Println(len("linux"))
+}
