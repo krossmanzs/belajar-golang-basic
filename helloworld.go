04ef3cbc8d65fb2ba54740ee171e651152a015a0
Krossmanzs <radi.adi78@gmail.com>
2023-01-06 16:31:40 +0700

    First commit

---
+++ helloworld.go
@@ -0,0 +1,14 @@
+package main
+
+// package fmt: formatter I/O
+import "fmt"
+
+/*
+	Untuk running langsung bisa menggunakan go run {file}
+	Untuk build go file bisa menggunakan go build {file}
+*/
+
+func main() {
+	fmt.Println("Helo world")
+	fmt.Println("This is my first time using Go-Lang")
+}
