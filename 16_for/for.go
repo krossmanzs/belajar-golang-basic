bb1571984245210cfbc71acdb87a9f7a55a24c25
Cornelius Linux <radi.adi78@gmail.com>
2023-04-21 01:43:45 +0700

    for loop

---
+++ 16_for/for.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for biasa
+	var counter int = 0
+	for counter <= 10 {
+		fmt.Println("Ini adalah perulangan ke", counter)
+		counter++
+	}
+
+	fmt.Println()
+	// for dengan statement
+	for i := 1; i <= 10; i++ {
+		fmt.Println("Perulangan ke", i)
+	}
+
+	fmt.Println()
+	// for range
+	// misal ada sebuah slice
+	names := []string{"Cornelius", "Linux", "Selebew", "Uwu"}
+
+	// jika manual
+	for index := 0; index < len(names); index++ {
+		fmt.Println(names[index])
+	}
+
+	fmt.Println()
+	// jika menggunakan for range
+	for index, name := range names { // penulisan index tidak wajib atau bisa diganti _
+		fmt.Println("Index:", index, "Name:", name)
+	}
+
+	person := make(map[string]string)
+	person["name"] = "Cornelius Linux"
+	person["age"] = "18"
+
+	for key, value := range person {
+		fmt.Println("Key:", key, "Value:", value)
+	}
+}
